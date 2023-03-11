a1b9624b50b47667c7ea8daf8f3f0fafcb31048e
bloodish <artemka0@outlook.com>
2023-03-11 20:50:56 +0300

    new method added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿string[] newArray = {"tfhj", "yyeuqui", "str", "uueuio", "qwe", "was"};
+﻿string[] newArray1 = new string[6] {"tfhj", "yyeuqui", "str", "uueuio", "qwe", "was"};
+string[] newArray2 = new string[newArray1.Length];
 
+void SizeCheck(string[] newArray1, string[] newArray2)
+{
+    int count = 0;
+    for (int i = 0; i < newArray1.Length; i++)
+    {
+        if(newArray1[i].Length <= 3)
+        {
+            newArray2[count] = newArray1[i];
+            count++;
+        }
+    }
+}
